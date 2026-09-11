ed31ed4b696b3b0815602ba9020e937efba3a012
arthzao <arthurmspa@gmail.com>
2026-09-11 11:04:09 -0300

    mudei o limite pra 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteadosToppe = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 mensagemInicialToppe();
 let numeroSecreto = gerarNumeroAleatorio();
 let numeroDeTentativas = 1;
@@ -25,7 +25,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function mensagemInicialToppe() {
 exibirTextoNaTela('h1', 'joguin do número secreto');
-exibirTextoNaTela('p', 'escolhe um número entre 1 e 10 por gentileza meu patrão?');
+exibirTextoNaTela('p', 'escolhe um número entre 1 e 100 por gentileza meu patrão?');
     };
 
 function verificarChute() {
