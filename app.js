0a0f424a9eae9d248a25f7b011c9da0f72c7806e
arthzao <arthurmspa@gmail.com>
2026-09-24 17:48:58 -0300

    sera, sera que o git vai ignorar o .txt????????

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function exibirTextoNaTela(tag, texto) {
 };
 
 function mensagemInicialToppe() {
-exibirTextoNaTela('h1', 'joguin do número çikret');
+exibirTextoNaTela('h1', 'joguin do número secreto');
 exibirTextoNaTela('p', 'escolhe um número entre 1 e 50 por gentileza meu patrão?');
     };
 
